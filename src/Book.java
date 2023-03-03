4b6e37e390cb85d8db5bd17cf9d6f636caadf766
ridex456 <kamil.poszwa@gmail.com>
2023-03-03 17:59:22 +0100

    Wgranie

---
+++ src/Book.java
@@ -0,0 +1,32 @@
+class Book {
+    String title;
+    String author;
+    int releaseDate;
+    int pages;
+    String publisher;
+    String isbn;
+
+    Book(String title, String author, int releaseDate, int pages, String publisher) {
+        this.title = title;
+        this.author = author;
+        this.releaseDate = releaseDate;
+        this.pages = pages;
+        this.publisher = publisher;
+    }
+
+    Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
+        this(title, author, releaseDate, pages, publisher);
+        this.isbn = isbn;
+    }
+
+    void printInfo() {
+
+        String info = title + "; " + author + "; " + releaseDate + "; " + pages + "; " + publisher + "; " + isbn;
+
+        System.out.println(info);
+
+    }
+
+
+
+}
---
+++ src/Library.java
@@ -0,0 +1,23 @@
+class Library {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka v0.4";
+
+        Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
+                296, "Greg", "9843423424243234");
+
+        Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch",
+                2009, 352, "Helion", "9843423424223234");
+
+        Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
+                "Ber Bates, Katherine Sierra", 2008, 851,
+                "McGraw-Hill Osborne Media");
+
+        System.out.println(appName);
+        System.out.println("Książki dostępne w bibliotece:");
+        book1.printInfo();
+        book2.printInfo();
+        book3.printInfo();
+    }
+}
+
+
