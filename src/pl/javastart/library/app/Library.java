eafae1cd413d398c2e5329322dae42b131e0328a
ridex456 <kamil.poszwa@gmail.com>
2023-03-04 18:49:50 +0100

    Wersja v0.6

--- src/pl/javastart/library/app/Library.java
+++ src/pl/javastart/library/app/Library.java
@@ -4,7 +4,7 @@ import pl.javastart.library.model.Book;
 
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.5";
+        final String appName = "Biblioteka v0.6";
 
         Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
                 296, "Greg", "9843423424243234");
